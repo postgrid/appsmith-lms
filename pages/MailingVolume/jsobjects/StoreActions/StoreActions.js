1340a325ddbacfc50b1581136be815e16b6dae5f
Bryan Genetia <bryan@postgrid.com>
2023-04-12 12:52:11 +0000

    adding mailing volume

--- pages/MailingVolume/jsobjects/StoreActions/StoreActions.js
+++ pages/MailingVolume/jsobjects/StoreActions/StoreActions.js
@@ -4,7 +4,12 @@ export default {
 	//	await Save_Data.run({currentRow});
 		//await get_printer_order_details.run(()			=> { showAlert('Well done!.','success')}, () => {});
 
-	} /*,
+	},runvolume: async () => {
+		await get_VolumeByPrinters.run()
+		
+	}
+	
+	/*,
 	myFun2: async (currentRow) => {
 		await storeValue("rowUpdate",currentRow);
 		await Save_Data2.run({currentRow});
