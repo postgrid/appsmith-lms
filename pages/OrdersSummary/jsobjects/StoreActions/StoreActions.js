03584ea27e1a1842f2c52467ab1f02c883480019
Bryan Genetia <bryan@postgrid.com>
2023-05-08 01:43:37 +0000

    Accounting dashboard added

--- pages/OrdersSummary/jsobjects/StoreActions/StoreActions.js
+++ pages/OrdersSummary/jsobjects/StoreActions/StoreActions.js
@@ -12,5 +12,14 @@ export default {
 		await get_customer_order_details.run();
 		await get_printer_order_details.run(()			=> { get_letter_day_volume.run(),
 get_cheque_day_volume.run(), get_postcard_day_volume.run(), showAlert('Well done!.','success')}, () => {});
+	},
+	
+	conversion: async () => {
+		storeValue('currency',get_forexchange.data.rates)
+	},
+			runvolume: async () => {
+ 		await get_pivot_expense.run();
+		await get_pivot_income.run();
+		
 	}
 }
