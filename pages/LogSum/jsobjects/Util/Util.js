8085921a5388b7d192e45bfde12337ae1a97905c
Bryan Genetia <bryan@postgrid.com>
2023-05-08 20:42:57 +0000

    custom rates for Printnow added

--- pages/LogSum/jsobjects/Util/Util.js
+++ pages/LogSum/jsobjects/Util/Util.js
@@ -7,21 +7,13 @@ export default {
 	synchronize: async () => {
 		await	get_user.run();
 		await	get_user_department.run();
-		await	get_current_tier.run();
-		await	get_customer_order_details.run();
+	
 		//await	get_invoicelist.run();
-		await	get_list_of_collaterals.run();
-		await	get_num_new_clients.run();
-		await	get_printer_order_details.run();
-		await	get_printerlist.run();
-		await get_cheque_day_volume.run();
-		await get_letter_day_volume.run();
-		await get_postcard_day_volume.run();
+		
 	},
 	autorefresh: async () => {
 		setInterval(() => {
-				get_letter_day_volume.run(),
-get_cheque_day_volume.run(), get_postcard_day_volume.run(),get_num_new_clients.run()}, 3000, "autoupdate");
+				 }, 3000, "autoupdate");
 /*get_invoicelist_count.run()*/
 	}
 
--- pages/Orders/jsobjects/StoreActions/StoreActions.js
+++ pages/Orders/jsobjects/StoreActions/StoreActions.js
@@ -14,6 +14,11 @@ export default {
 		await get_printer_order_details.run(()			=> { get_letter_day_volume.run(),
 get_cheque_day_volume.run(), get_postcard_day_volume.run(), showAlert('Well done!.','success')}, () => {});
 	},	
+		myFun3: async (currentRow) => {
+		await storeValue("rowUpdate",currentRow);
+		await Save_Data3.run({currentRow});
+		await get_printer_order_details.run(()			=> {  showAlert('Well done!.','success')}, () => {});
+	},	
 	rundateoforder: async () => {
  		 await get_invoicelist.run(()=>{
 		 get_PauseCancel_custOrderDetai.run(),
