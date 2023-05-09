50a07f0404f6448229ba126d12660cef7fdbf68f
Bryan Genetia <bryan@postgrid.com>
2023-05-09 14:00:23 +0000

    mailing volume update

--- pages/MailingVolume/jsobjects/StoreActions/StoreActions.js
+++ pages/MailingVolume/jsobjects/StoreActions/StoreActions.js
@@ -5,8 +5,10 @@ export default {
 		//await get_printer_order_details.run(()			=> { showAlert('Well done!.','success')}, () => {});
 
 	},runvolume: async () => {
-		await get_VolumeByPrinters.run()
-		
+		await get_VolumeByPrinters.run();
+		await get_EOD_Summary.run();
+		await get_mailing_volume.run();
+		await get_VolumeByPrinters.run();
 	}
 	
 	/*,
