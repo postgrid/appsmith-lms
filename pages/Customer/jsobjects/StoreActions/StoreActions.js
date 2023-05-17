6eeeac9c0e4b64a31f2fbae8f0c07722ae81c9fe
Bryan Genetia <bryan@postgrid.com>
2023-05-17 21:59:12 +0000

    Price dashboard update

--- pages/Customer/jsobjects/StoreActions/StoreActions.js
+++ pages/Customer/jsobjects/StoreActions/StoreActions.js
@@ -8,12 +8,19 @@ export default {
 	myFun2: async (currentRow) => {
 		await storeValue("rowUpdate",currentRow);
 	},
+		runselect: async () => {
+		resetWidget('Select1')
+		resetWidget('Table3')
+			Query1.run()
+		StoreActions.runQuery('Query1')
+	},
 	runQuery: async (queryName) => {
 		const queries = {
 		"Query1": Query1,
 		"Query2": Query2
 		}
 		await queries[queryName]?.run();
+		//Table3.tableData = queries[queryName].data
 		await storeValue('tableData', queries[queryName].data)
 	}
 }
--- pages/Customer/jsobjects/Util/Util.js
+++ pages/Customer/jsobjects/Util/Util.js
@@ -7,19 +7,9 @@ export default {
 	synchronize: async () => {
 		await	get_user.run();
 		await	get_user_department.run();
-		await	get_current_tier.run();
-		await	get_customer_order_details.run();
-		await	get_invoicelist.run();
-		await	get_list_of_collaterals.run();
-		await	get_num_new_clients.run();
-		await	get_printer_order_details.run();
-		await	get_printerlist.run();
-		await get_cheque_month_volume.run();
-		await get_letter_month_volume.run();
-		await get_postcard_month_volume.run();
-  },
+  }/*,
 	autorefresh: async () => {
 		setInterval(() => {get_invoicelist.run(),get_num_new_clients.run(),Query1.run(),get_custpricelist.run()}, 2000, "autoupdate");
-  }
+  }*/
 			
 }
