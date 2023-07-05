482aa6ba8014b5bd6da5eaf1e7f5d7ba7c162de0
jordan <jordan@postgrid.com>
2023-07-05 19:49:37 +0000

    Logistic Summary Table Clean up

--- pages/LogSum/jsobjects/StoreActions/StoreActions.js
+++ pages/LogSum/jsobjects/StoreActions/StoreActions.js
@@ -7,7 +7,6 @@ export default {
 			let extraSheetPrice = 0;
 			let extraSheetQty = 0;
 			const rates = [];
-			// console.log("JG lineItems", lineItems)
 
 			for(const lineItem of lineItems){
 				if(lineItem.rate === null){
@@ -47,17 +46,15 @@ export default {
 		};
 
 		await get_invoicelist.run(()=>{
-			get_allorders_invoice.run(),
+				get_allorders_invoice.run(),
 				get_allorder_items.run(),
 				get_allorder_printcost.run(),
 				get_customer_price_info.run(),
-				get_printer_price_info.run(),
-				showAlert('Order date updated!', 'success')
+				get_printer_price_info.run()
 		}, () => {});
 		const combinedData = [];
 		await storeValue('logSumTableProgress', 'loading...');
-		console.log("JG get_customer_price_info", get_customer_price_info.data)
-		for(const item of Object.entries(get_allorders_invoice.data)){
+		for(const item of get_allorders_invoice.data){
 			const tier2 = get_allorder_items.data.find((item2) => item.Id == item2.Id) || {};
 			const customerLineItems = get_customer_price_info.data.filter(order => order.Id === item.Id || order.SubItemID === item.Id)
 
@@ -65,12 +62,11 @@ export default {
 			const tier3 = get_allorder_printcost.data.find((item3) => item.Id == item3.Id) || {};
 			const printerLineItems = await get_printer_price_info.data.filter(order => order.Id === item.Id || order.SubItemID === item.Id);
 			const calcPrinter = !tier3 || JSON.stringify(tier3) === '{}'  ? '' : buildCalculation(printerLineItems, tier3.PSummary);
-			// console.log("JG", item, tier2, tier3, calcCustomer, calcPrinter)
 			combinedData.push({ ...item, ...tier2 ,...tier3, calcCustomer, calcPrinter});
 		}
-		console.log("JG combined", combinedData)
 
 		await storeValue('logSummary',combinedData);
 		await storeValue('logSumTableProgress', '');
+		showAlert('Order date updated!', 'success');
 	},
 }
